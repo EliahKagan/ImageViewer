cec1dcf63078903c4f0d6b6bb4bf95d50b7c61c4
Eliah Kagan <degeneracypressure@gmail.com>
2021-04-27 08:25:31 -0400

    start in home dir (all OSes); show message box on open/decode failure

--- viewer.py
+++ viewer.py
@@ -6,6 +6,8 @@ from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from PySide2.QtCore import *
 
+MAX_START_RATIO = 0.9
+
 parser = argparse.ArgumentParser('Image Viewer')
 parser.add_argument('path',
                     nargs='?',
@@ -13,22 +15,33 @@ parser.add_argument('path',
                     type=str)
 path = parser.parse_args().path
 
+print(f'{sys.argv[0]}: PID {os.getpid()}', file=sys.stderr)
+
 app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle('Image Viewer')
 
 screenBounds = app.desktop().screenGeometry(window)
-maxWidth = math.floor(screenBounds.width() * 0.9)
-maxHeight = math.floor(screenBounds.height() * 0.9)
+maxWidth = math.floor(screenBounds.width() * MAX_START_RATIO)
+maxHeight = math.floor(screenBounds.height() * MAX_START_RATIO)
 
 if path is None:
     path, _ = QFileDialog.getOpenFileName(window,
                                           'Open Image File',
-                                          r'D:\Downloads')
+                                          os.path.expanduser('~'))
     if not path:
-        sys.exit(1)
+        sys.exit(1)  # If the user cancels out of the dialog, just quit.
 
 picture = QPixmap(path)
+
+if picture.isNull():
+    dialog = QMessageBox(window)
+    dialog.setWindowTitle(window.windowTitle())
+    dialog.setText(f"Couldn't open {path}")
+    dialog.setWindowFlag(Qt.WindowStaysOnTopHint)
+    dialog.exec_()
+    sys.exit(1)
+
 if picture.width() > maxWidth or picture.height() > maxHeight:
     picture = picture.scaled(maxWidth, maxHeight, Qt.KeepAspectRatio)
 
