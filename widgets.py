6f52f97418d4766589e353cae035d9d7a8f84ab0
Eliah Kagan <degeneracypressure@gmail.com>
2021-04-28 12:42:51 -0400

    used Qt signal/slot to handle resize

--- widgets.py
+++ widgets.py
@@ -1,15 +1,14 @@
 """Custom widgets for Image Viewer."""
 
+from PySide6.QtCore import Signal
 from PySide6.QtGui import QResizeEvent
 from PySide6.QtWidgets import QWidget
 
-class ResizeNotifyingWidget(QWidget):
+class ResizedNotifyingWidget(QWidget):
     """A widget with a resize handler that can be set at runtime."""
 
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.onResize = lambda _: None
+    resized = Signal(QResizeEvent)
 
     def resizeEvent(self, event: QResizeEvent) -> None:
         super().resizeEvent(event)
-        self.onResize(event)
+        self.resized.emit(event)
