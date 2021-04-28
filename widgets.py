83d40997e4d3b5712652c6af62e445f618f2447a
Eliah Kagan <degeneracypressure@gmail.com>
2021-04-28 08:21:05 -0400

    resizes, but with wrong stretching behavior

--- widgets.py
+++ widgets.py
@@ -4,13 +4,12 @@ from PySide2.QtGui import QResizeEvent
 from PySide2.QtWidgets import QWidget
 
 class ResizeNotifyingWidget(QWidget):
-    """A widget with a reisze handler that can be set at runtime."""
+    """A widget with a resize handler that can be set at runtime."""
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.onResize = lambda _: None
 
     def resizeEvent(self, event: QResizeEvent) -> None:
-        print('Resizing.')  # FIXME: remove after debugging
         super().resizeEvent(event)
         self.onResize(event)
